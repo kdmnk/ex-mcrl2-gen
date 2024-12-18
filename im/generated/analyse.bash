4565da75f84a85c45888156d23180afac50af3d4
kdmnk <katkodominik@gmail.com>
2024-12-18 14:56:33 +0100

    receive based mcrl2 to be generated

--- im/generated/analyse.bash
+++ im/generated/analyse.bash
@@ -1,14 +1,14 @@
-export PATH=$PATH:~/Projects/mCRL2/stage/mCRL2.app/Contents/bin
+export PATH=$PATH:/Applications/mCRL2.app/Contents/bin
 mcrl22lps "$1.mcrl2" "$1.lps" -l regular2
 echo "lps generated"
 lps2lts "$1.lps" "$1.lts"
 echo "lts generated"
 lps2pbes -v -c -f "$1.mcf" "$1.lps" "$1.pbes"
 echo "pbes generated"
-pbes2bool -v "$1.pbes"
+#pbes2bool -v "$1.pbes"
 #LpsXSim "$1.lps"
 pbessolve -v --file="$1.lts" "$1.pbes"
-ltsgraph "$1.pbes.evidence.lts"
+#ltsgraph "$1.pbes.evidence.lts"
 #ltsgraph "$1.lts"
 
 
